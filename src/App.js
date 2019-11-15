be01cf187ccaa0d9a46ff5848ee622a409cb42a5
Rahim Khan <akarahim@hotmail.com>
2019-11-16 01:37:32 +0530

    render simple card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import ComplaintViewCard from './components/ComplaintCard';
 
+const renderCards = () =>
+  ['1', '2', '3'].map(x => (
+    <>
+      <br />
+      <ComplaintViewCard />
+    </>
+  ));
 function App() {
   return (
     <>
       <NavBar />
-      <h1>Rahim</h1>
+      {renderCards()}
     </>
   );
 }
